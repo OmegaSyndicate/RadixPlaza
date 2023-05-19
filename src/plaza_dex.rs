037d5cc4e61199cd1acf8ad86aeceeddc6ae388c
Jazzer9F <Jazzer9F@users.noreply.github.com>
2023-05-19 20:51:04 +0200

    Added swap function to DEX

--- src/plaza_dex.rs
+++ src/plaza_dex.rs
@@ -24,6 +24,7 @@ mod plazadex {
             .globalize()
         }
 
+        // Create a new liquidity pair on the exchange
         pub fn create_pair(&mut self, tokens: Bucket, dfp2: Bucket, p0: Decimal) -> (Bucket, Bucket) {
             // Verify dfp2 tokens are indeed dfp2 tokens
             assert!(dfp2.resource_address() == self.dfp2, "Need to add DFP2 liquidity");
@@ -31,7 +32,8 @@ mod plazadex {
             // Ensure the pair doesn't exist yet
             let rri = tokens.resource_address();
             assert!(self.token_to_pair.contains_key(&rri) == false, "Pair already exists");
-        
+            assert!(rri != self.dfp2, "Can't add DFP2 as base token");
+            
             // Create new pair
             let (pair, lp_base, lp_quote) = PlazaPairComponent::instantiate_pair(tokens, dfp2, p0);
 
@@ -43,6 +45,37 @@ mod plazadex {
             (lp_base, lp_quote)
         }
 
+        // Swap tokens
+        pub fn swap(&mut self, tokens: Bucket, output_token: ResourceAddress) -> Bucket {
+            let input_token = tokens.resource_address();
+
+            // Verify tokens can be traded at the exchange
+            assert!(input_token != output_token, "Can't swap token into itself");
+            assert!(self.token_to_pair.contains_key(&input_token) || input_token == self.dfp2, "Input token not listed");
+            assert!(self.token_to_pair.contains_key(&output_token) || output_token == self.dfp2, "Output token not listed");
+
+            match (input_token == self.dfp2, output_token == self.dfp2) {
+                (true, _) => {
+                    // Sell DFP2 (single pair trade)
+                    let pair = &self.token_to_pair[&output_token];
+                    pair.swap(tokens)
+                }
+                (_, true) => {
+                    // Buy DFP2 (single pair trade)
+                    let pair = &self.token_to_pair[&input_token];
+                    pair.swap(tokens)
+                }
+                _ => {
+                    // Trade two tokens with a hop through DFP2
+                    let pair1 = &self.token_to_pair[&input_token];
+                    let pair2 = &self.token_to_pair[&output_token];
+                    let dfp2_bucket = pair1.swap(tokens);
+                    pair2.swap(dfp2_bucket)
+                }
+            }
+        }
+
+        // Get a quote for swapping tokens
         pub fn quote(&self, input_token: ResourceAddress, input_amount: Decimal, output_token: ResourceAddress) -> Decimal {
             // Verify tokens are all traded at the exchange
             assert!(input_token != output_token, "Can't swap token into itself");
@@ -51,17 +84,17 @@ mod plazadex {
 
             match (input_token == self.dfp2, output_token == self.dfp2) {
                 (true, _) => {
-                    // Sell DFP2 from a single pair
+                    // Sell DFP2 (single pair trade)
                     let pair = &self.token_to_pair[&output_token];
                     pair.quote(input_amount, true).0
                 }
                 (_, true) => {
-                    // Buy DFP2 to a single pair
+                    // Buy DFP2 (single pair trade)
                     let pair = &self.token_to_pair[&input_token];
                     pair.quote(input_amount, false).0
                 }
                 _ => {
-                    // Trade using a hop through DFP2
+                    // Trade two tokens with a hop through DFP2
                     let pair1 = &self.token_to_pair[&input_token];
                     let pair2 = &self.token_to_pair[&output_token];
                     let dfp2_amount = pair1.quote(input_amount, false).0;
